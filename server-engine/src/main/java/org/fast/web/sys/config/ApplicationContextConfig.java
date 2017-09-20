7c9eca0fdd630b237cae3252c2e59be4a0d37631
FL <504096193@qq.com>
2017-09-20 15:12:19 +0800

    first check out

--- server-engine/src/main/java/org/fast/web/sys/config/ApplicationContextConfig.java
+++ server-engine/src/main/java/org/fast/web/sys/config/ApplicationContextConfig.java
@@ -1,7 +1,11 @@
 package org.fast.web.sys.config;
 
+import org.fast.web.model.SpringScanTag;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 /**
- * Description:  BillcardinoutManager
+ * Description:  spring上下文配置类
  * Copyright: © 2017 CSNT. All rights reserved.
  * Company: CSNT
  *
@@ -9,5 +13,7 @@ package org.fast.web.sys.config;
  * @version 1.0
  * @timestamp 2017/9/20
  */
+@Configuration
+@ComponentScan(basePackageClasses = {SpringScanTag.class})
 public class ApplicationContextConfig {
 }
