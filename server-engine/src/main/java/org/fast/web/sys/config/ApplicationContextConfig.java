ed14e25f9f19f7a808ac7261a27bea3a248a32a4
FL <504096193@qq.com>
2017-09-20 15:04:31 +0800

    first check out

---
+++ server-engine/src/main/java/org/fast/web/sys/config/ApplicationContextConfig.java
@@ -0,0 +1,13 @@
+package org.fast.web.sys.config;
+
+/**
+ * Description:  BillcardinoutManager
+ * Copyright: © 2017 CSNT. All rights reserved.
+ * Company: CSNT
+ *
+ * @author 付尧
+ * @version 1.0
+ * @timestamp 2017/9/20
+ */
+public class ApplicationContextConfig {
+}
