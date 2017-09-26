a1df67d57d59164743539c731f2db528bc3a6966
FL <504096193@qq.com>
2017-09-26 16:55:45 +0800

    first check out

--- server-engine/src/main/java/org/fast/web/sys/config/CacheConfig.java
+++ server-engine/src/main/java/org/fast/web/sys/config/CacheConfig.java
@@ -1,10 +1,13 @@
 package org.fast.web.sys.config;
 
-import org.springframework.cache.CacheManager;
+import net.sf.ehcache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 /**
  * Description:  CacheConfig
@@ -19,8 +22,16 @@ import org.springframework.context.annotation.Configuration;
 @EnableCaching
 public class CacheConfig {
     @Bean
-    public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager();
+    public EhCacheCacheManager cacheManager(CacheManager cm) {
+
+        return new EhCacheCacheManager(cm);
+    }
+
+    @Bean
+    public EhCacheManagerFactoryBean ehcache() {
+        EhCacheManagerFactoryBean ehCacheManagerFactoryBean = new EhCacheManagerFactoryBean();
+        ehCacheManagerFactoryBean.setConfigLocation(new ClassPathResource("org/fast/web"));
+        return ehCacheManagerFactoryBean;
     }
 
 }
