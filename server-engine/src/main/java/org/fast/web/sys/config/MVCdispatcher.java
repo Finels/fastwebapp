84a548ecaea2a24c93ffa0d268ac1c613006b2db
FL <504096193@qq.com>
2017-09-30 16:26:31 +0800

    first check out

---
+++ server-engine/src/main/java/org/fast/web/sys/config/MVCdispatcher.java
@@ -0,0 +1,29 @@
+package org.fast.web.sys.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * Description:  BillcardinoutManager
+ * Copyright: © 2017 CSNT. All rights reserved.
+ * Company: CSNT
+ *
+ * @author 付尧
+ * @version 1.0
+ * @timestamp 2017/9/30
+ */
+public class MVCdispatcher extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
