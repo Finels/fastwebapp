45a09cda5447537f75da2ec5bbfe49291f984a27
FL <504096193@qq.com>
2018-01-11 10:41:55 +0800

    可以发布的稳定版本

--- server-engine/src/main/java/org/fast/web/sys/exception/Bizexception.java
+++ server-engine/src/main/java/org/fast/web/sys/exception/Bizexception.java
@@ -1,42 +1,101 @@
 package org.fast.web.sys.exception;
 
+import org.springframework.http.HttpStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * Description:  Bizexception
+ * Description:  BizException
  * Copyright: © 2017 CSNT. All rights reserved.
  * Company: CSNT
  *
  * @author 付尧
  * @version 1.0
- * @timestamp 2017/11/8
+ * @timestamp 2017/12/19
  */
-public class Bizexception extends RuntimeException {
+public class BizException extends RuntimeException {
+    private String apiName;
+    private String exceptionMsg;
     private String errorCode;
-    private String errorDescription;
+    private HttpStatus httpStatus;
+    private Map contextMap;
 
-    public Bizexception(String errorCode) {
+    public BizException(String apiName) {
         super(new Exception());
-        this.errorCode = errorCode;
-        this.errorDescription = "未知错误。";
+        this.apiName = apiName;
+        this.exceptionMsg = "";
+        this.contextMap = new HashMap();
     }
 
-    public Bizexception(String errorCode, String errorDescription) {
+    public BizException(String apiName, Map contextMap) {
+        super(new Exception());
+        this.apiName = apiName;
+        this.exceptionMsg = "";
+        this.contextMap = contextMap;
+    }
+
+    public BizException(String apiName, String exceptionMsg, String errorCode, HttpStatus httpStatus, Map contextMap) {
+        super(new Exception());
+        this.apiName = apiName;
+        this.exceptionMsg = exceptionMsg;
         this.errorCode = errorCode;
-        this.errorDescription = errorDescription;
+        this.httpStatus = httpStatus;
+        this.contextMap = contextMap;
+    }
+
+    public BizException(String apiName, String errorCode, String exceptionMsg, HttpStatus httpStatus) {
+//        super(new Exception());
+        this.apiName = apiName;
+        this.exceptionMsg = exceptionMsg;
+        this.errorCode = errorCode;
+        this.httpStatus = httpStatus;
+    }
+
+    public BizException(String apiName, Map contextMap, Throwable t) {
+        super(t);
+        this.apiName = apiName;
+        this.exceptionMsg = "";
+        this.contextMap = contextMap;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    public void setHttpStatus(HttpStatus httpStatus) {
+        this.httpStatus = httpStatus;
+    }
+
+    public String getApiName() {
+        return apiName;
+    }
+
+    public void setApiName(String apiName) {
+        this.apiName = apiName;
+    }
+
+    public Map getContextMap() {
+        return this.contextMap;
+    }
+
+    public void setContextMap(Map contextMap) {
+        this.contextMap = contextMap;
+    }
+
+    public String getExceptionMsg() {
+        return this.exceptionMsg;
+    }
+
+    public void setExceptionMsg(String exceptionMsg) {
+        this.exceptionMsg = exceptionMsg;
     }
 
     public String getErrorCode() {
-        return errorCode;
+        return this.errorCode;
     }
 
     public void setErrorCode(String errorCode) {
         this.errorCode = errorCode;
-    }
-
-    public String getErrorDescription() {
-        return errorDescription;
-    }
-
-    public void setErrorDescription(String errorDescription) {
-        this.errorDescription = errorDescription;
     }
 }
